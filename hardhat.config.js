4652cc08bf94e936f6c38344b2c310f1af1dd505
Jase Mwaura <rawscript170@gmail.com>
2025-11-20 23:23:27 -0800

    fix Polygon amoy smart contract established

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    amoy: {
+      url: process.env.BLOCKCHAIN_RPC_URL || "https://rpc-amoy.polygon.technology",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002,
+    }
+  }
 };
